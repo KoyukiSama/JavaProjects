ed05e8de7bc0db28523f60c835fc88dafd98dd38
KoyukiSama <119341682+KoyukiSama@users.noreply.github.com.>
2024-09-09 22:01:39 +0200

    add findTaskById() deleteTask() updateDescription
    findTaskById() fetches the task searched by id to then perform deletetask or updateDescription

--- TaskTracker/TaskTracker/src/TaskManager.java
+++ TaskTracker/TaskTracker/src/TaskManager.java
@@ -15,13 +15,50 @@ public class TaskManager {
     public void addTask(String description) { // add task
         int id = tasks.size() + 1;
         tasks.add( new Task(id, description) );
+        System.out.println("Success!\n");
+    }
+
+    public void updateTaskDescription(int id, String description) { // add description by id
+        Task task = findTaskById(id);
+        if (task == null) {
+            System.out.println("Error: couldn't find id.");
+        } else {
+            task.setDescription(description);
+            System.out.println("Success!\n");
+        }
+    }
+    
+    public void deleteTask(int id) { // delete by id
+        Task task = findTaskById(id);
+        if (task == null) {
+            System.out.println("Error: couldn't find id.");
+        } else {
+            tasks.remove(task);
+            System.out.println("Success!\n");
+        }
+    }
+    public void clearTasks() {
+        for (Task task : tasks) {
+            tasks.remove(task);
+            System.out.println("Success!\n");
+        }
+    }
+
+    
+    public Task findTaskById(int id) { // find task by id
+        for (Task task : tasks) {
+            if (task.getId() == id) {
+                return task;
+            }
+        }
+        return null;
     }
 
     public String linebreakStart = "--------\\";
     public String linebreakEnd = "--------/";
-    public String label = "id   status   description   updated   created\n\n"; //label placed above data
+    public String label = "id   status   description   updated   created"; //label placed above data
     public void listTasks() { // list tasks
-        System.out.println(linebreakStart+" Tasks\n"+label);
+        System.out.println(linebreakStart+" Tasks\n"+label+"\n");
         for (Task task : tasks) {
             System.out.println(task);
         }
