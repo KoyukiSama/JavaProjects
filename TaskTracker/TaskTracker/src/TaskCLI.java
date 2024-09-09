70116ec68c4de776862939dc475422cfbe13cbe8
KoyukiSama <119341682+KoyukiSama@users.noreply.github.com.>
2024-09-10 00:32:26 +0200

    fix
    fixed some type bugs
    strenghtened error handling in switch statement by adding if else to check for null and -1;

--- TaskTracker/TaskTracker/src/TaskCLI.java
+++ TaskTracker/TaskTracker/src/TaskCLI.java
@@ -3,16 +3,17 @@ public class TaskCLI {
     public static void main(String[] args) {
         TaskManager manager = new TaskManager();
 
-        argc = args.length;
+        int argc = args.length;
+        String syntax = "Syntax: task-cli ... ...\nadd \"description\"\nupdate id \"description\"\ndelete id\n\nmark-in-progress id\nmark-done id\nmark-todo id\n\nlist --: listing shows the ids\nlist in-progress\nlist todo\nlist in-progress";
         if (argc == 0) {
-            System.out.println("Syntax: task-cli ... ...\nadd \"description\"\nupdate id \"description\"\ndelete id\n\nmark-in-progress id\nmark-done id\nmark-todo id\n\nlist --: listing shows the ids\nlist in-progress\nlist todo\nlist in-progress");
+            System.out.println(syntax);
             return;
         }
 
         String command = args[0];
-        String attribute1;
-        int attribute1id;
-        String attribute2;
+        String attribute1 = null;
+        int attribute1id = -1;
+        String attribute2 = null;
 
         if (argc > 1) { // prevent touching memory that isn't that there.
             attribute1 = args[1]; 
@@ -27,20 +28,36 @@ public class TaskCLI {
 
         switch (command) {
             case "add": // add update delete
-                manager.addTask(attribute1);                
+                if (attribute1 != null) {
+                    manager.addTask(attribute1);                
+                } else {
+                    System.out.println("Error: Description is required for 'add' command.");
+                }
                 break;
             case "update":
-                manager.updateTaskDescription(attribute1id, attribute2);
+                if (attribute1id != -1 && attribute2 != null) {
+                    manager.updateTaskDescription(attribute1id, attribute2);
+                } else {
+                    System.out.println("Error: Valid ID and description are required for 'update' command.");
+                }
                 break;
             case "delete":
-                manager.deleteTask(attribute1id);
+                if (attribute1id != -1) {
+                    manager.deleteTask(attribute1id);
+                } else {
+                    System.out.println("Error: Valid ID is required for 'delete' command.");
+                }
                 break;
 
 
             case "mark-inprogress": // marking
             case "mark-done":
             case "mark-todo":
-                manager.markStatus(command, attribute1id);                
+                if (attribute1id != -1) {
+                    manager.markStatus(command, attribute1id);                
+                } else {
+                    System.out.println("Error: Valid ID is required for '" + command + "' command.");
+                }
                 break;
 
 
@@ -52,7 +69,7 @@ public class TaskCLI {
                 }
                 break;
             default:
-                System.out.println("unknown error occured in TaskCLI switch statement");
+                System.out.println("unknown error occured in TaskCLI switch statement\n\n"+syntax);
                 break;
         }
     }
