68b81ef46a4e799ecab4ef9ece3667e87af58e7d
KoyukiSama <119341682+KoyukiSama@users.noreply.github.com.>
2024-09-14 17:25:37 +0200

    add printGrid and populateGrid

--- AppleGirl/src/AppleGirlGame.java
+++ AppleGirl/src/AppleGirlGame.java
@@ -1,11 +1,23 @@
 public class AppleGirlGame {
 
     private int width; // fields
-    private int height; // fields
+    private int height;
+    private char[][] grid;
 
     public AppleGirlGame(int width, int height) {
         this.width = width;
         this.height = height;
+        this.grid = new char[height][width];
+
+        popGrid();
+    }
+
+    private void popGrid() { // populates grid
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                grid[y][x] = ' ';
+            }
+        }
     }
 
     ////// getters and setters //////
@@ -15,9 +27,25 @@ public class AppleGirlGame {
     public int getWidth() {
         return width;
     }
+    public char[][] getGrid() {
+        return grid;
+    }
 
+    ////// game start ///////
 
     public static void main(String[] args) {
-        
+
+    }
+
+
+    ////// extra game logic /////
+
+    public void printGrid() {
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                System.out.print(grid[y][x]);
+            }
+            System.out.println("");
+        }
     }
 }
