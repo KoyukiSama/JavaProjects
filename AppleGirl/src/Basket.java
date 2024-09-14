65769c615ae1c59d329572ba977bc0d7d90fed21
KoyukiSama <119341682+KoyukiSama@users.noreply.github.com.>
2024-09-14 16:25:16 +0200

    add goLeft and goRight

--- AppleGirl/src/Basket.java
+++ AppleGirl/src/Basket.java
@@ -1,10 +1,11 @@
-public class Basket {
+public class Basket implements Runnable {
 
     private int x = 0; // fields
     private int y = 0;
     private static char V = 'V';
     private boolean leftKey = false;
     private boolean rightKey = false;
+    private int moveSpeed = 200;
 
     public Basket(int x, int y) { // constructor
         this.x = x;
@@ -13,5 +14,43 @@ public class Basket {
 
 
     ////// logic methods //////
+
+    // goLeft
+    @Override
+    public void run() {   // 
+        while (leftKey || rightKey) {
+            try {
+                if (leftKey) {
+                    goLeft();
+                } else if (rightKey) {
+                    goRight();
+                }
+                Thread.sleep(moveSpeed);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                break;
+            }
+        }
+    }
+
+    public void goLeft() { // moving input logic
+        x--;
+    }
+    public void goRight() {
+        x++;
+    }
+
+
+    // goRight
+
+
+    /////// Setters and getters //////
+
+    public int getX() {
+        return x;
+    }
+    public int getY() {
+        return y;
+    }
 
 }
