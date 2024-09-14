55e0fd2748a8ffac17e50049bb1a1f95f2c50177
KoyukiSama <119341682+KoyukiSama@users.noreply.github.com.>
2024-09-14 16:39:34 +0200

    add width and height to constructor

--- AppleGirl/src/AppleGirlGame.java
+++ AppleGirl/src/AppleGirlGame.java
@@ -1,4 +1,14 @@
 public class AppleGirlGame {
+
+    private int width; // fields
+    private int height; // fields
+
+    public AppleGirlGame(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+
     public static void main(String[] args) {
 
     }
