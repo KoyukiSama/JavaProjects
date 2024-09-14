97152b1d60ea83181c38d984b121e0490ce808a9
KoyukiSama <119341682+KoyukiSama@users.noreply.github.com.>
2024-09-14 16:09:00 +0200

    add fields and constructors

---
+++ AppleGirl/src/Basket.java
@@ -0,0 +1,17 @@
+public class Basket {
+
+    private int x = 0; // fields
+    private int y = 0;
+    private static char V = 'V';
+    private boolean leftKey = false;
+    private boolean rightKey = false;
+
+    public Basket(int x, int y) { // constructor
+        this.x = x;
+        this.y = y;
+    }
+
+
+    ////// logic methods //////
+
+}
