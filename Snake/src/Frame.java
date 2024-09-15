91a226c877ceeb2e4cd1da40b63735998e2f14a9
KoyukiSama <119341682+KoyukiSama@users.noreply.github.com.>
2024-09-16 00:03:43 +0200

    add score setter

--- Snake/src/Frame.java
+++ Snake/src/Frame.java
@@ -1,19 +1,37 @@
 import java.awt.Color;
+import java.awt.Font;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class Frame extends JFrame {
+    private JLabel scoreLabel;
+
     public Frame() {
 
+        ///// FRAME //////
 		this.setTitle("Snake Game");
 		//this.setIconImage();     // needs import javax.swing.ImageIcon
 
 		this.setSize(500, 500);
-		this.setVisible(true);
 
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setResizable(false);
 
 		this.getContentPane().setBackground(new Color(0xfecdb8));
+
+        ///// JPanel /////
+        JPanel topBar = new JPanel();
+
+        ///// JLABELS /////
+        scoreLabel = new JLabel("SCORE: 0");
+        scoreLabel.setFont(new Font("Monospaced", Font.BOLD, 18));
+
+        topBar.add(scoreLabel);
+
+        this.add(topBar);
+
+        this.setVisible(true);
 
 	}
 
