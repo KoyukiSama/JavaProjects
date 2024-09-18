3d6f7fb03b665fa0380670195556bf27fc5ecbc0
KoyukiSama <119341682+KoyukiSama@users.noreply.github.com.>
2024-09-18 14:09:19 +0200

    add getOppositeKey()

--- SnakeMaven/src/main/java/com/example/Key.java
+++ SnakeMaven/src/main/java/com/example/Key.java
@@ -1,5 +1,27 @@
 package com.example;
 
 public enum Key {
-    UP, RIGHT, DOWN, LEFT
+    UP, RIGHT, DOWN, LEFT;
+
+
+    public static Key getOppositeKey(Key key) {
+        Key opposite = Key.RIGHT;
+        switch (key) {
+            case UP:
+                opposite = Key.DOWN;
+                break;
+            case RIGHT:
+                opposite = Key.LEFT;
+                break;
+            case DOWN:
+                opposite = Key.UP;
+                break;
+            case LEFT:
+                opposite = Key.RIGHT;
+                break;
+            default:
+                break;
+        }
+        return opposite;
+    }
 }
