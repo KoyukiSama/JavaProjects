2433e6408833e2c214428ee879917a912c4995d5
KoyukiSama <119341682+KoyukiSama@users.noreply.github.com.>
2024-09-20 15:09:10 +0200

    add getStrLevel and getLevel

--- SnakeMaven/src/main/java/com/example/Score.java
+++ SnakeMaven/src/main/java/com/example/Score.java
@@ -1,5 +1,18 @@
 package com.example;
 
 public class Score {
-    
+    private int score;
+   
+    public Score() {
+        this.score = 0;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public String getStrScore() {
+        String strScore = Integer.toString(score);
+        return strScore;
+    }
 }
